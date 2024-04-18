54c3a3673c07201f399be8545cf4aba7d3818a38
JESUS ALBERTO FUNES GARCIA <jesus.funes21@itca.edu.sv>
2024-04-18 16:22:58 -0600

    Add fontawesome dependencies and update banner layout

--- src/app/components/BannerPages.tsx
+++ src/app/components/BannerPages.tsx
@@ -12,8 +12,8 @@ export const BannerPages = ({ perritoImg, separador, title, text }: BannerPagesP
   return (
     <>
       <div className="banner-container z-0">
-        <div className="d-flex banner-home m-auto w-75 ">
-          <div className="col-6 d-flex justify-content-center align-items-center flex-column p-5 text-start">
+        <div className="d-flex flex-md-row flex-column-reverse row banner-home m-auto w-md-75 w-100 flex-wrap-reverse">
+          <div className="col-md-6 col p-md-5 px-3 pb-5 d-flex justify-content-center align-items-center flex-column text-md-start text-center">
             <div className="w-100">
               <p className="text-warning mb-0 mx-3"><b>PetMatch</b></p>
             <h1> { title } </h1>
@@ -21,8 +21,8 @@ export const BannerPages = ({ perritoImg, separador, title, text }: BannerPagesP
              <button className="btn btn-warning btn-link-a"><Link to={ "/adopta" }>Ver los perritos</Link></button>
             </div>
           </div>
-          <div className="col-6 p-5 d-flex justify-content-end ">
-            <img className="w-75" src={ perritoImg } alt="Perro" />
+          <div className="col-md-6 col p-md-5 px-5 d-flex justify-content-md-end justify-content-center max-w-500">
+            <img className="w-100 h-auto object-fit-cover" src={ perritoImg } alt="Perro" />
           </div>
         </div>
       </div>
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,10 +1,12 @@
 
 import { NavLink } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 export const Header = () => {
   return (
-    <header>
-      <div className='container-header'>
+    <header className=''>
+      <div className='container-header desktop-header d-md-flex d-hidden'>
         <div className='col-6 d-flex justify-content-start '>
         <NavLink to='/' className='link-head'> PetMatch </NavLink>
         </div>
@@ -14,6 +16,13 @@ export const Header = () => {
           <NavLink to='/perfil' className='link-head'> Perfil </NavLink>
           <NavLink to='/login' className='link-head'> Ingresar </NavLink>
         </div>
+      </div>
+      <div className='container-header mobile-header md-hidden'>
+        <div className='col-6 d-flex justify-content-between'>
+        <NavLink to='/' className='link-head'> PetMatch </NavLink>
+        <a className='cursor-pointer'> <FontAwesomeIcon icon={faBars} /> </a>
+        </div>
+        
       </div>
       
     </header>
