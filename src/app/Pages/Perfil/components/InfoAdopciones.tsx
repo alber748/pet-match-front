8781e8cb2e6e047d904e29b4229efed71a3d7342
JESUS ALBERTO FUNES GARCIA <jesus.funes21@itca.edu.sv>
2024-04-22 01:07:06 -0600

    add post Dog

--- src/app/Pages/Perfil/components/InfoAdopciones.tsx
+++ src/app/Pages/Perfil/components/InfoAdopciones.tsx
@@ -1,113 +1,162 @@
-import { useState } from "react"
-
-
+import { ChangeEvent, FormEvent, useState } from "react";
+import axios from "axios";
 
 //Components
-import { CardPerro } from "../../../components/CardPerro"
-//Assets 
-import perroEjemplo from "../../../../assets/perro-ejemplo.jpeg"
+import { CardPerro } from "../../../components/CardPerro";
+//Assets
+import perroEjemplo from "../../../../assets/perro-ejemplo.jpeg";
+import { Dog } from "../../../models/Dog";
 
 interface Iadopciones {
-    title: string,
-    agregarPerros?: () => void;
+  title: string;
+  agregarPerros?: () => void;
 }
 
 const catalogo = [
-    {
-        id: 1,
-        nombre: 'Harry',
-        edad: 2,
-        raza: 'Chihuahua',
-        fotos: [
-            perroEjemplo,
-            'https://images.dog.ceo/breeds/chihuahua/n02085620_10131.jpg',
-            'https://images.dog.ceo/breeds/chihuahua/n02085620_10158.jpg'
-        ],
-        descripcion: 'Es un perro muy jugueton y cariñoso'
-    },
-    {
-        id: 2,
-        nombre: 'Firulais',
-        edad: 4,
-        raza: 'Pastor Aleman',
-        fotos: [
-            perroEjemplo,
-            'https://images.dog.ceo/breeds/germanshepherd/n02106662_10131.jpg',
-            'https://images.dog.ceo/breeds/germanshepherd/n02106662_10147.jpg'
-        ],
-        descripcion: 'Es un perro muy jugueton y cariñoso'
-    },
-    {
-        id: 3,
-        nombre: 'Pirata',
-        edad: 3,
-        raza: 'Labrador',
-        fotos: [
-            perroEjemplo,
-            'https://images.dog.ceo/breeds/labrador/n02099712_10131.jpg',
-            'https://images.dog.ceo/breeds/labrador/n02099712_10147.jpg'
-        ],
-        descripcion: 'Es un perro muy jugueton y cariñoso'
-    },
-    {
-        id: 4,
-        nombre: 'Pirata',
-        edad: 3,
-        raza: 'Labrador',
-        fotos: [
-            perroEjemplo,
-            'https://images.dog.ceo/breeds/labrador/n02099712_10131.jpg',
-            'https://images.dog.ceo/breeds/labrador/n02099712_10147.jpg'
-        ],
-        descripcion: 'Es un perro muy jugueton y cariñoso'
-    }, {
-        id: 5,
-        nombre: 'Pirata',
-        edad: 3,
-        raza: 'Labrador',
-        fotos: [
-            perroEjemplo,
-            'https://images.dog.ceo/breeds/labrador/n02099712_10131.jpg',
-            'https://images.dog.ceo/breeds/labrador/n02099712_10147.jpg'
-        ],
-        descripcion: 'Es un perro muy jugueton y cariñoso'
-    }
-]
+  {
+    id: 1,
+    nombre: "Harry",
+    edad: 2,
+    raza: "Chihuahua",
+    fotos: [
+      perroEjemplo,
+      "https://images.dog.ceo/breeds/chihuahua/n02085620_10131.jpg",
+      "https://images.dog.ceo/breeds/chihuahua/n02085620_10158.jpg",
+    ],
+    descripcion: "Es un perro muy jugueton y cariñoso",
+  },
+  {
+    id: 2,
+    nombre: "Firulais",
+    edad: 4,
+    raza: "Pastor Aleman",
+    fotos: [
+      perroEjemplo,
+      "https://images.dog.ceo/breeds/germanshepherd/n02106662_10131.jpg",
+      "https://images.dog.ceo/breeds/germanshepherd/n02106662_10147.jpg",
+    ],
+    descripcion: "Es un perro muy jugueton y cariñoso",
+  },
+  {
+    id: 3,
+    nombre: "Pirata",
+    edad: 3,
+    raza: "Labrador",
+    fotos: [
+      perroEjemplo,
+      "https://images.dog.ceo/breeds/labrador/n02099712_10131.jpg",
+      "https://images.dog.ceo/breeds/labrador/n02099712_10147.jpg",
+    ],
+    descripcion: "Es un perro muy jugueton y cariñoso",
+  },
+  {
+    id: 4,
+    nombre: "Pirata",
+    edad: 3,
+    raza: "Labrador",
+    fotos: [
+      perroEjemplo,
+      "https://images.dog.ceo/breeds/labrador/n02099712_10131.jpg",
+      "https://images.dog.ceo/breeds/labrador/n02099712_10147.jpg",
+    ],
+    descripcion: "Es un perro muy jugueton y cariñoso",
+  },
+  {
+    id: 5,
+    nombre: "Pirata",
+    edad: 3,
+    raza: "Labrador",
+    fotos: [
+      perroEjemplo,
+      "https://images.dog.ceo/breeds/labrador/n02099712_10131.jpg",
+      "https://images.dog.ceo/breeds/labrador/n02099712_10147.jpg",
+    ],
+    descripcion: "Es un perro muy jugueton y cariñoso",
+  },
+];
 
 export const InfoAdopciones = ({ title }: Iadopciones) => {
-    const [mostrasPerros, setMostrarPerros] = useState(false)
-    const [isPublicacion, setIsPublicacion] = useState(false)
+    const [mostrasPerros, setMostrarPerros] = useState(false);
+    const [isPublicacion, setIsPublicacion] = useState(false);
+    const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
+    const [formData, setFormData] = useState<Dog>({
+        id: 0,
+        idPersona: 0,
+        name: '',
+        edad: '',
+        peso: '',
+        files: [],
+        descripcion: '',
+        situacion: ''
+    });
 
     const handleMostrarPerros = (): void => {
-        setMostrarPerros(prevState => !prevState)
-    }
-    const handlePublicacion = (): void => {
-        setIsPublicacion(prevState => !prevState)
-    }
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const form = event.currentTarget;
-        const files = Array.from(form.elements).filter((element): element is HTMLInputElement => (
-            element instanceof HTMLInputElement && element.type === 'file'
-        )).map((input: HTMLInputElement) => input.files?.[0]);
-
-
-        console.log(files); 
-
+        setMostrarPerros(prevState => !prevState);
     };
-    const handleform = () => {
+
+    const handlePublicacion = (): void => {
+        setIsPublicacion(prevState => !prevState);
+    };
+
+    const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files) {
+            const filesArray = Array.from(event.target.files);
+            setSelectedFiles(prevFiles => [...prevFiles, ...filesArray]);
+        }
+    };
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        const formDataToSend = new FormData();
+        selectedFiles.forEach((file) => {
+            formDataToSend.append('files', file);
+        });
+
+        // Agregar los nuevos campos al FormData
+        formDataToSend.append('name', formData.name);
+        formDataToSend.append('idPersona', String(formData.idPersona));
+        formDataToSend.append('edad', formData.edad);
+        formDataToSend.append('peso', formData.peso);
+        formDataToSend.append('descripcion', formData.descripcion);
+        formDataToSend.append('situacion', formData.situacion);
+
+        console.log('Formulario a enviar:', formDataToSend);
+        try {
+            const response = await axios.post('https://pet-match-backend.onrender.com/api/dogs', formDataToSend);
+            console.log(response.data);
+        } catch (error) {
+            console.error('Error al enviar archivos:', error);
+        }
+    };
+
+    const renderForm = () => {
         if (!mostrasPerros) {
             if (isPublicacion) {
                 return (
                     <form onSubmit={handleSubmit}>
-                        <input type="file" />
-                        <input type="file" />
-                        <input type="file" />
-                        <button type="submit" > enviar</button>
-                    </form>
-                )
-            } else {
+                        <input type="file" onChange={handleFileChange} />
+                        <input type="file" onChange={handleFileChange} />
+                        <input type="file" onChange={handleFileChange} />
+                        {/* Nuevos campos */}
+                        <input type="text" name="name" placeholder="Nombre" onChange={handleChange} />
+                        <input type="text" name="idPersona" placeholder="ID Persona" onChange={handleChange} />
+                        <input type="text" name="edad" placeholder="Edad" onChange={handleChange} />
+                        <input type="text" name="peso" placeholder="Peso" onChange={handleChange} />
+                        <input type="text" name="descripcion" placeholder="Descripción" onChange={handleChange} />
+                        <input type="text" name="situacion" placeholder="Situación" onChange={handleChange} />
 
+                        <button type="submit">Enviar</button>
+                    </form>
+                );
+            } else {
                 return (
                     <>
                         {catalogo.slice(0, 4).map((perro) => (
@@ -124,17 +173,17 @@ export const InfoAdopciones = ({ title }: Iadopciones) => {
                 catalogo.map((perro) => (
                     <CardPerro key={perro.id} perro={perro} />
                 ))
-            )
+            );
         }
-    }
-
+    };
 
     return (
         <div>
             <h1>{title}<span onClick={handlePublicacion}>+</span></h1>
             <div className={`d-flex align-items-center p-3 gap-5  ${!mostrasPerros ? "overflow-hidden" : "flex-wrap"}`}>
-                {handleform()}
+                {renderForm()}
             </div>
         </div>
-    )
-}
+    );
+};
+
---
+++ src/app/models/Dog.ts
@@ -0,0 +1,10 @@
+export interface Dog {
+      id: number;
+      idPersona : number;
+      name: string;
+      edad: string;
+      peso: string;
+      files: File[];
+      descripcion: string;
+      situacion: string;
+}
