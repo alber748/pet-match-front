4e3eca463eeff6380ba8c9b9fa1f80aecc2bdcd0
Toojeronix <manajeronimo7@gmail.com>
2024-04-22 15:05:55 -0300

    Info de usuario

--- src/app/Pages/Perfil/components/InfoAdopciones.tsx
+++ src/app/Pages/Perfil/components/InfoAdopciones.tsx
@@ -6,6 +6,7 @@ import { CardPerro } from "../../../components/CardPerro";
 import { ModalPublicacion } from "./ModalPublicacion"
 //Assets
 import perroEjemplo from "../../../../assets/perro-ejemplo.jpeg";
+import axios from "axios";
 
 
 interface Iadopciones {
@@ -88,14 +89,30 @@ export const InfoAdopciones = ({ title }: Iadopciones) => {
         setModalFormAddPerros(prevState => !prevState)
     };
 
+    const renderMisPublicaiones = async () => {
+        const token = localStorage.getItem('token')
+        if (token) {
+            const uId = localStorage.getItem('idUser')
+            try {
+                const response = await axios.get(`https://pet-match-backend.onrender.com/api/dogs/get?uId=${uId}`)
+                console.log(response.data);
+
+            } catch (error) {
+                console.error(" no se encontro nada ", error)
+            }
+        }
+        else {
+            console.error("Inicie secion")
+        }
+
+    }
+
 
     const rendercards = () => {
         if (mostrasPerros) {
             return (
                 <>
-                    {catalogo.map((perro) => (
-                        <CardPerro key={perro.id} perro={perro} />
-                    ))}
+                    {renderMisPublicaiones()}
                     <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="#bb7b04" className="bi bi-arrow-left-circle" viewBox="0 0 16 16" onClick={handleMostrarPerros}>
                         <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z" />
                     </svg>
@@ -107,6 +124,7 @@ export const InfoAdopciones = ({ title }: Iadopciones) => {
                     {catalogo.slice(0, 4).map((perro) => (
                         <CardPerro key={perro.id} perro={perro} />
                     ))}
+                    {renderMisPublicaiones()}
                     <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="#bb7b04" className="bi bi-arrow-right-circle" viewBox="0 0 16 16" onClick={handleMostrarPerros}>
                         <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z" />
                     </svg>
--- src/app/Pages/Perfil/components/InformacionPerfil.tsx
+++ src/app/Pages/Perfil/components/InformacionPerfil.tsx
@@ -1,8 +1,35 @@
 import { NavLink } from "react-router-dom"
 //Assets
 import logo from "../../../../assets/logo-user.png"
+import { useState, useEffect } from 'react';
 
 export const InformacionPerfil = () => {
+    const [infoUser, setInfoUser] = useState({
+        email: "",
+        name: "",
+        lastname: "",
+        phone: "",
+        location: "",
+    })
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            const user = localStorage.getItem('users');
+            if (user) {
+                const arregloUsuarios = JSON.parse(user);
+                if (Array.isArray(arregloUsuarios) && arregloUsuarios.length > 0) {
+                    const primerUsuario = arregloUsuarios[0];
+                    setInfoUser({
+                        ...infoUser,
+                        ...primerUsuario
+                    });
+                } else {
+                    console.log('El arreglo de usuarios está vacío.');
+                }
+            }
+        }
+    }, []);
+
     return (
         <div className=" d-flex justify-content-center my-5 p-3 container-info-perfil w-100">
             <div className="h-100 mt-5  cont-img">
@@ -14,11 +41,11 @@ export const InformacionPerfil = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="#bb7b04" width={17} height={17}><path d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6 .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4 191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z" /></svg>
                     </NavLink>
                 </div>
-                <h1 className="me-3">Jerónimo Mana</h1>
+                <h1 className="me-3">{`${infoUser.name} ${infoUser.lastname}`}</h1>
                 <div className="mt-3 info-top">
-                    <p>Email: <span>manajeronimo7@gmail.com</span></p>
-                    <p>Telefono: <span>3516830927</span></p>
-                    <p>Ubicacion: <span>Argentina,Córdoba</span></p>
+                    <p>Email: <span>{infoUser.email}</span></p>
+                    <p>Telefono: <span>{infoUser.phone}</span></p>
+                    <p>Ubicacion: <span>{infoUser.location}</span></p>
                 </div>
                 <div className=" mt-3 info-bottom">
                     <p>Tipo de entidad: <span>Persona</span></p>
